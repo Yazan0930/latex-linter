dca494ced3e9c13615afc77844efcc7cd48089d1
Yazan0930 <mohamadyazanbardakani@gmail.com>
2022-10-06 17:30:55 +0200

    changed config.json path

--- functions.py
+++ functions.py
@@ -101,13 +101,13 @@ def linterLatex(f, betterGitSupport, lineBreakSize):
 
 def updateConfig(jsonData, key, value):
     jsonData["custom"][key] = value
-    jsonFile = open("config.json", "w")
+    jsonFile = open("/Users/mohamadyazanbardakani/bin/config.json", "w")
     jsonFile.write(json.dumps(jsonData))
     jsonFile.close()
 
 # get json data
 def getJsonData():
-    jsonFile = open("config.json")
+    jsonFile = open("/Users/mohamadyazanbardakani/bin/config.json")
     jsonData = json.load(jsonFile)
     jsonFile.close()
     return jsonData
