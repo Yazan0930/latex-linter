cdeae188f7cb090305421c975fcfe4404778b1ea
Yazan0930 <mohamadyazanbardakani@gmail.com>
2022-10-13 23:24:05 +0200

    uninstall

--- uninstall-macos.bash
+++ uninstall-macos.bash
@@ -4,7 +4,7 @@
 rm -rf $HOME/app/latex-linter
 
 # remove $HOME/bin from $PATH
-sed -i '' '/export PATH=$HOME\/bin\/latex-linter:$PATH/d' $HOME/.bash_profile
+sed -i '' '/export PATH=$HOME\/app\/latex-linter:$PATH/d' $HOME/.bash_profile
 
 # remove alias latex-linter
 sed -i '' '/alias latex-linter='index.py'/d' $HOME/.bash_profile
