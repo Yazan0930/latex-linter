809eda0b7149483e49a93ce2f716d7bfc420b95d
Yazan0930 <mohamadyazanbardakani@gmail.com>
2022-10-13 23:14:11 +0200

    install

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# latex-linter
-A simple latex linter built python
+to import a tex file from the host to docker continer
+use: docker cp file.name 369e644036bf:/root/y
--- install_macos.bash
+++ install_macos.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Create folder $HOME/bin
-mkdir -p $HOME/bin
+mkdir -p $HOME/app
 # pull code from github to $HOME/bin
-git clone https://github.com/Yazan0930/latex-linter $HOME/bin/latex-linter
+git clone https://github.com/Yazan0930/latex-linter $HOME/app/latex-linter
 
 # add $HOME/bin to $PATH
 echo "export PATH=$HOME/bin/latex-linter:$PATH" >> $HOME/.bash_profile
@@ -17,11 +17,8 @@ echo "alias latex-linter='index.py'" >> $HOME/.zshrc
 # create alias latex-linter to $HOME/bin/index.py
 echo "alias latex-linter='index.py'" >> ~/.bash_profile
 
-# reload bash profile
-source ~/.bash_profile
-
 # give permission to execute index.py
-chmod +x $HOME/bin/latex-linter/index.py
+chmod +x $HOME/app/latex-linter/index.py
 
 # reload bash profile
 source ~/.bash_profile
