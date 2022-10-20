897d2de2a200c4442386e8a14cb7c1cc9ac87210
Yazan0930 <mohamadyazanbardakani@gmail.com>
2022-10-20 21:49:27 +0200

    pytest

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -3,16 +3,25 @@ import functions
 def test_add_taps():
     string = "This is a test"
     assert functions.addTaps(string, 1) == "\tThis is a test"
+    # print before and after changes to see if it works
+    print("Before: \n", string)
+    print("\nAfter: \n", functions.addTaps(string, 1))
 
 def test_new_lines():
     string = "This is a test"
     #if condition returns True, then nothing happens:
     assert functions.addNewLines(string, 1) == "\nThis is a test"
+    print("Before: \n", string)
+    print("\nAfter: \n", functions.addNewLines(string, 1))
 
 def test_break_up_sentences():
     string = "This is a test. This is a test."
     assert functions.breakUpSentences(string) == "This is a test.\n This is a test.\n"
+    print("Before: \n", string)
+    print("\nAfter: \n", functions.breakUpSentences(string))
 
 def test_space_after_comment():
     string = "%This is a test"
     assert functions.addSpaceInComment(string) == "% This is a test"
+    print("Before: \n", string)
+    print("\nAfter: \n", functions.addSpaceInComment(string))
